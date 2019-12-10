30576a7fc1a2b01dfe64a11fb180706c6b5c4e74
SE/2017/010 <ekanayak_se17010@kln.ac.lk>
2019-12-10 13:38:21 +0530

    folder added

---
+++ MobilePhone/cmake-build-debug/CMakeFiles/MobilePhone.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MobilePhone.dir/main.c.obj"
+  "MobilePhone.exe"
+  "MobilePhone.exe.manifest"
+  "MobilePhone.pdb"
+  "libMobilePhone.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/MobilePhone.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
